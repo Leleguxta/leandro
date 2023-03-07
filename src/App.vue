5e45e77b9f651b900b489bb73d314d38b260796f
Leleguxta <leandrogustavosantos.info@gmail.com>
2023-03-07 11:54:32 -0300

    aula2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
+const rawHtml = '<span style="color: red">Este é um texto em vermelho</span>'
+
 const contador = ref(0)
 
 function incrementarContador() {
@@ -20,6 +22,13 @@ function reset(){
 onMounted(() => {
   console.log(`O valor inicial do contador é ${contador.value}.`)
 })
+
+const numero = 1
+const ok = true
+const mensagem = 'programar'
+function inverter(texto) {
+    return texto.split('').reverse().join('')
+}
 </script>
 
 <template>
@@ -27,6 +36,11 @@ onMounted(() => {
   <button @click="decrementarContador">-</button>
   <button @click="reset">/</button>
   <p>Valor do contador é: {{ contador }}</p>
+  <p>Usando interpolação de textos: {{rawHtml}}</p>
+  <p>Usando v-html: <span v-html="rawHtml"></span></p>
+  <p>{{ numero + 1 }}</p>
+  <p>{{ ok ? 'Sim' : 'Não' }}</p>
+  <p>{{ mensagem.split('').reverse().join('') }}</p>
 </template>
 
 <style scoped>
